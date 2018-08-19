145313318e032f9f2308bc07cdc82a7a15ff896c
GodPete <Petermarcelis95@gmail.com>
2018-08-19 18:22:13 +0200

    sprite stuff

---
+++ Assets/Scripts/CharacterAnimatorController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterAnimatorController : MonoBehaviour {
+
+	private Animator animator;
+
+	private void Awake()
+	{
+		animator = this.GetComponent<Animator>();
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey(KeyCode.A))
+		{
+			animator.SetBool("isWalking", true);
+		}
+		else
+		{
+			animator.SetBool("isWalking", false);
+		}
+	}
+}
